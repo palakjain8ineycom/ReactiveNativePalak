d9b463d699f772a7b11efae086e0e1fb068820f0
nikhitaarun04 <Nikhita.Arun@in.ey.com>
2023-05-04 15:20:38 +0530

    calender-nikhita

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import { Animated, View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Image,TouchableOpacity, Modal } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+// import { AntDesign } from "@expo/vector-icons";
+import 'react-native-gesture-handler';
+import { MaterialIcons } from "@expo/vector-icons";
+import Calendar from "react-native-calendars/src/calendar";
+
+
+
+console.reportErrorsAsExceptions = false;
 
 function Logo() {
   return <Image source={require("./assets/logo.png")} style={styles.logo} />;
@@ -14,80 +24,235 @@ function TopBar() {
   );
 }
 
-const HomeScreen = ({ navigation }) => {
+function LeaveForm() {
+  const [showStartModal, setShowStartModal] = useState(false);
+  const [showEndModal, setShowEndModal] = useState(false);
+  const [employeeNumber, setEmployeeNumber] = useState("");
+  const [leaveType, setLeaveType] = useState("");
+  const [reason, setReason] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+
+  const renderArrow = (direction) => (
+    <MaterialIcons
+      name={direction === "left" ? "chevron-left" : "chevron-right"}
+      size={30}
+      color="black"
+    />
+  );
+
+
+  const handleSubmit = () => {
+    fetch("https://fa-equm-test-saasfaprod1.fa.ocs.oraclecloud.com/hcmRestApi/resources/11.13.18.05/absences", 
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        employeeNumber: employeeNumber,
+        leaveType: leaveType,
+        reason: reason,
+        startDate: startDate,
+        endDate: endDate,
+      }),
+    })
+      // .then((response) => response.json())
+      // .then((data) => {
+      //   Alert.alert("Your Leave has been submitted successfully");
+      // })
+      // .catch((error) => {
+      //   Alert.alert(error.message);
+      // });
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.tile} onPress={() => navigation.navigate('Leave Application')}>
-        <Text style={styles.tileText}>Leave Application</Text>
+      <TopBar />
+      <View style={styles.form}>
+        <Text style={styles.label}>Employee Number *</Text>
+        <TextInput
+          style={styles.input}
+          value={employeeNumber}
+          onChangeText={setEmployeeNumber}
+          keyboardType="numeric"
+          placeholder="Enter employee number"
+          maxLength={10}
+          required
+        />
+        <Text style={styles.label}>Leave Type</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={leaveType}
+          onValueChange={(itemValue) => setLeaveType(itemValue)}
+        >
+          <Picker.Item label="Select Leave Type" value="" />
+          <Picker.Item
+            label="Absence Regularization"
+            value="Absence Regularization"
+          />
+          <Picker.Item label="Bereavement Leave" value="Bereavement Leave" />
+          <Picker.Item label="COVID Leave" value="COVID Leave" />
+          <Picker.Item
+            label="Casual cum Sick Leave (CL)"
+            value="Casual cum Sick Leave (CL)"
+          />
+          <Picker.Item
+            label="Comp off - Accrual / Credit"
+            value="Comp off - Accrual / Credit"
+          />
+          <Picker.Item label="Loss of Pay" value="Loss of Pay" />
+          <Picker.Item label="On Duty" value="On Duty" />
+          <Picker.Item
+            label="Privilege Leave (PL)"
+            value="Privilege Leave (PL)"
+          />
+          <Picker.Item label="Relocation" value="Relocation" />
+          <Picker.Item label="Special Leave" value="Special Leave" />
+        </Picker>
+
+        <Text style={styles.label}>Reason</Text>
+        <TextInput
+          style={styles.input}
+          value={reason}
+          onChangeText={setReason}
+          maxLength={100}
+          placeholder="Enter leave reason"
+          required
+        />
+<Text style={styles.label}>Start Date</Text>
+<TouchableOpacity
+        onPress={() => setShowStartModal(true)}
+        style={styles.selectDateButton}
+      >
+        <Text style={styles.selectDateButtonText}>
+          {startDate ? `${startDate}` : "Enter the Start Date"}
+        </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={[styles.tile, styles.chatbotTile]} onPress={() => navigation.navigate('ChatBot')}>
-        <Image source={require("./assets/splash.png")} style={styles.chatbotImage} />
+<Text style={styles.label}>End Date</Text>
+      <TouchableOpacity
+        onPress={() => setShowEndModal(true)}
+        style={styles.selectDateButton}
+      >
+        <Text style={styles.selectDateButtonText}>
+          {endDate ? ` ${endDate}` : "Enter the End Date "}
+        </Text>
       </TouchableOpacity>
+
+
+
+        <Button
+          title="Submit"
+          onPress={handleSubmit}
+          color="#000000"
+          // Set the color prop to the desired color
+        />
+<Modal visible={showStartModal} animationType="fade">
+        <Calendar
+          style={styles.calendar}
+          renderArrow={renderArrow}
+          onDayPress={(day) => {
+            setStartDate(day.dateString);
+            setShowStartModal(false);
+          }}
+        />
+      </Modal>
+      <Modal visible={showEndModal} animationType="fade">
+        <Calendar
+          style={styles.calendar}
+          renderArrow={renderArrow}
+          onDayPress={(day) => {
+            setEndDate(day.dateString);
+            setShowEndModal(false);
+          }}
+        />
+      </Modal>
+      </View>
     </View>
   );
-};
-
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5F5F5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   topBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 40,
-    marginBottom: 20,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFFff",
+    padding: 16,
   },
   logo: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  tile: {
-    width: '80%',
-    height: 150,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginVertical: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tileText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#1E90FF',
-  },
-  chatbotTile: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    backgroundColor: '#1E90FF',
-    borderRadius: 50,
     width: 70,
     height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
-  chatbotImage: {
-    width: 40,
-    height: 40,
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginTop: 16,
+    color: "#000000",
+  },
+  form: {
+    padding: 16,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 8,
+    color: "#000000",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+    color: "#575756ff",
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: "#575756ff",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+    color: "#575756ff",
+  },
+  submitBtn: {
+    padding: 8,
+    borderRadius: 4,
+
+  },
+  inputFocused: {
+    borderColor: "yellow",
+  },
+  calendar: {
+   
+    elevation: 4,
+    margin: 50,
+    height: 10,
+    width: 250,
+    alignContent:"center",
+    fontWeight: 'bold',
+  },
+  Picker:{
+    height: 50,
+    width: "100%",
+    borderColor: "gray",
+    marginTop: 20,
+    padding: 10,
+
+  },
+  selectDateButton: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+    color: "#575756ff",
+
+  },
+  selectDateButtonText: {
+    color: "black",
+    fontSize: 15,
+
   },
 });
+export default LeaveForm;
 
-export default HomeScreen;
